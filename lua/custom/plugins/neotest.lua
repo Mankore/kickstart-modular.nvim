eacc75f6662f7ab0aad147aa0660b865752be33b
Mankor <test@mankor.com>
2025-04-11 17:11:36 +0200

    refactor: Adjust neotest config to resolve paths correctly in monorepos

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -10,10 +10,27 @@ return {
   config = function()
     local neotest = require 'neotest'
 
+    local function find_nearest_node_modules_dir()
+      local current_dir = vim.fn.expand '%:p:h' -- current buffer dir
+      while current_dir ~= '/' do
+        if vim.fn.isdirectory(current_dir .. '/node_modules') == 1 then
+          return current_dir
+        end
+        current_dir = vim.fn.fnamemodify(current_dir, ':h')
+      end
+      return nil
+    end
+
+    local function get_root_dir()
+      local original_cwd = vim.fn.getcwd()
+      local node_modules_dir = find_nearest_node_modules_dir()
+      return node_modules_dir or original_cwd
+    end
+
     neotest.setup {
       adapters = {
         require 'neotest-vitest' {
-          -- cwd = get_root_dir, -- <- do not call func here - remove ()
+          cwd = get_root_dir,
           filter_dir = function(name, rel_path, root)
             return name ~= 'node_modules'
           end,
