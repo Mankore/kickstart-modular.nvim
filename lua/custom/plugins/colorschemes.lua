fa63922278934bc4992b1f54c3f9cac75d3d821b
Mankor <test@mankor.com>
2025-04-17 17:02:52 +0200

    chore: Clean up

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,46 +1,41 @@
 return {
   {
-    {
-      'catppuccin/nvim',
-      name = 'catppuccin',
-      priority = 1000,
-      config = function()
-        require('catppuccin').setup({
-          flavour = 'macchiato', -- latte, frappe, macchiato, mocha
-          transparent_background = false,
-          term_colors = false,
-        })
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'macchiato', -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        term_colors = false,
+      })
 
-        -- vim.cmd.colorscheme 'catppuccin'
-      end,
-    },
-    {
-      'sainnhe/everforest',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.everforest_background = 'hard'
-        vim.g.everforest_cursor = 'green'
+      -- vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = 'hard'
+      vim.g.everforest_cursor = 'green'
 
-        -- vim.cmd.colorscheme 'everforest'
-      end,
-    },
-    {
-      'sainnhe/gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        -- vim.g.gruvbox_material_foreground = 'mix'
+      -- vim.cmd.colorscheme('everforest')
+    end,
+  },
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_float_style = 'dim'
+      vim.g.gruvbox_material_ui_contrast = 'hard'
 
-        vim.g.gruvbox_material_background = 'hard'
-        vim.g.gruvbox_material_enable_italic = true
-        vim.g.gruvbox_material_transparent_background = 0
-        vim.g.gruvbox_material_float_style = 'dim'
-        vim.g.gruvbox_material_ui_contrast = 'hard'
-        vim.cmd.colorscheme('gruvbox-material')
-      end,
-    },
+      vim.cmd.colorscheme('gruvbox-material')
+    end,
   },
 }
