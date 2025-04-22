42238969927527642c2490841bd070332cbc2b60
Mankor <test@mankor.com>
2025-04-22 09:50:15 +0200

    feat: Show current time in lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,11 +2,17 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local function getTime()
+      local time = os.date('*t')
+      return string.format('%02d:%02d:%02d', time.hour, time.min, time.sec)
+    end
+
     require('lualine').setup({
       options = {
         theme = 'codedark',
       },
       sections = {
+        lualine_x = { getTime, 'encoding', 'fileformat', 'filetype' },
         lualine_c = { { 'filename', path = 4 } },
       },
     })
