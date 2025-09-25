7853b9b159ad00916ee33df7abbd290317bc2f76
Mankor <test@mankor.com>
2025-09-25 09:50:24 +0200

    refactor: Change auto-session cmds

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -7,8 +7,8 @@ return {
   },
   keys = {
     -- Will use Telescope if installed or a vim.ui.select picker otherwise
-    { '<leader>wr', '<cmd>SessionSearch<CR>', desc = 'Session search' },
-    { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
+    { '<leader>wr', '<cmd>AutoSession search<CR>', desc = 'Session search' },
+    { '<leader>ws', '<cmd>AutoSession save<CR>', desc = 'Save session' },
     { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
   },
 }
